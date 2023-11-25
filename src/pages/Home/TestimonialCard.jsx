b9e0e2b710a1a017b1b5448480ed81eafac8dd50
pktatari <tawhidalmuhaimin@gmail.com>
2023-11-25 19:04:41 +0600

    minor color changes

--- src/pages/Home/TestimonialCard.jsx
+++ src/pages/Home/TestimonialCard.jsx
@@ -1,12 +1,12 @@
 import { Avatar, Box, Card, CardContent, Typography } from "@mui/material";
-
+import PropTypes from 'prop-types';
 
 
 
 const TestimonialCard = ({ review }) => {
     return (
-        <div className="w-[360px] p-3">
-            <Card sx={{ color: 'white', bgcolor: '#032e4b' }}>
+        <div className="w-[320px] p-3">
+            <Card sx={{ color: 'white', bgcolor: '#b79537' }}>
                 <CardContent sx={{ display: 'flex', flexDirection: "column", justifyContent: "space-around", height: "full" }}>
                     <Typography variant="h1" sx={{ fontFamily: "monospace", mb: -6 }}>“</Typography>
                     <Typography variant="body2">
@@ -19,19 +19,19 @@ const TestimonialCard = ({ review }) => {
                             alt="green iguana"
                         />
                         <Box sx={{ flexGrow: 1, }}>
-                            <Typography gutterBottom variant="body2" component="div" color={'gray'}  >
-                                written by
+                            <Typography gutterBottom variant="body2" component="div" color={'black'}  >
+                                Written by
                             </Typography>
-                            <Typography gutterBottom variant="h5" component="div" >
+                            <Typography gutterBottom variant="h5" component="div" color={"primary"} >
                                 {review.reviewerName}
                             </Typography>
                         </Box>
                     </Box>
 
-                    <Typography gutterBottom variant="body1" component="div" color={'gray'}  >
+                    <Typography gutterBottom variant="body2" component="div" color={'black'}  >
                         Review for:
                     </Typography>
-                    <Typography gutterBottom variant="h6" component="div" color='secondary'  >
+                    <Typography gutterBottom variant="subtitle2" component="div" color="primary"  >
                         {review.propertyTitle}
                     </Typography>
                 </CardContent>
@@ -39,6 +39,10 @@ const TestimonialCard = ({ review }) => {
             </Card>
         </div >
     );
+};
+
+TestimonialCard.propTypes = {
+    review: PropTypes.object,
 };
 
 export default TestimonialCard;
