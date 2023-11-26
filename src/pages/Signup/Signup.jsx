eb1544d84b6b19b7f7c26c068f5273dd06df6614
pktatari <tawhidalmuhaimin@gmail.com>
2023-11-26 15:14:34 +0600

    reg form validation done

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,5 +1,5 @@
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { Button, Card, CardActions, CardContent, Container, FormControl, Grid, IconButton, Input, InputAdornment, InputLabel, TextField, Typography } from "@mui/material";
+import { Alert, Avatar, Button, Card, CardActions, CardContent, Container, FormControl, Grid, IconButton, Input, InputAdornment, InputLabel, TextField, Typography } from "@mui/material";
 import { styled } from '@mui/material/styles';
 import { motion } from "framer-motion"
 import { useState } from "react";
@@ -21,6 +21,75 @@ const VisuallyHiddenInput = styled('input')({
 
 
 const Signup = () => {
+    const [name, setName] = useState('');
+    const [nameOk, setNameOk] = useState(true);
+    const [password, setPassword] = useState('');
+    const [passwordOk, setPasswordOk] = useState(true);
+    const [email, setEmail] = useState('');
+    const [emailOk, setEmailOk] = useState(true);
+    const [image, setImage] = useState(null);
+    const [imageOk, setImageOk] = useState(true);
+    const [imagePreview, setImagePreview] = useState(null);
+
+    const handleImageChange = (e) => {
+        setImage(e.target.files[0]);
+        setImagePreview(URL.createObjectURL(e.target.files[0]));
+        if (e.target.files) {
+            setImageOk(true);
+        }
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+        // eslint-disable-next-line no-useless-escape
+        const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]).{6,}$/;
+        setPasswordOk(passwordRegex.test(e.target.value));
+        if (!e.target.value) {
+            setPasswordOk(true);
+        }
+    };
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+        if (e.target.value) {
+            setNameOk(true);
+        }
+    };
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        setEmailOk(emailRegex.test(e.target.value));
+        if (!e.target.value) {
+            setEmailOk(true);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        // console.log(name, password, email);
+        e.preventDefault();
+        if (!name || !nameOk) {
+            setNameOk(false);
+            return
+        }
+        if (!image || !imageOk) {
+            setImageOk(false);
+            return
+        }
+        if (!email || !emailOk) {
+            setEmailOk(false);
+            return
+        }
+        if (!password || !passwordOk) {
+            setPasswordOk(false);
+            return
+        }
+
+        console.log(name, password, email);
+        console.log(image);
+
+    }
+
     const [showPassword, setShowPassword] = useState(false);
     const handleClickShowPassword = () => setShowPassword((show) => !show);
     const handleMouseDownPassword = (event) => {
@@ -29,7 +98,7 @@ const Signup = () => {
 
     return (
         <div>
-            <motion.div className=" bg-cover h-[100vh] flex flex-col justify-center
+            <motion.div className=" bg-cover min-h-[100vh] flex flex-col justify-center
             bg-gradient-to-bl  from-[#032e4b] from-40% via-emerald-800  to-[#032e4b] animate-gradient
             ">
                 <Container maxWidth="xl" sx={{ alignItems: "center", justifyContent: "center" }}>
@@ -43,20 +112,63 @@ const Signup = () => {
                             <Card sx={{ width: "90%" }}>
                                 <CardContent>
                                     <form
+                                        onSubmit={handleSubmit}
                                         style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: 'center' }}
                                         noValidate
                                         autoComplete="off"
                                     >
-                                        <AccountCircleIcon sx={{ width: 150, height: 150 }} />
+                                        {image ?
+                                            <Avatar
+                                                alt=""
+                                                src={imagePreview}
+                                                sx={{ width: 150, height: 150, mb: 2 }}
+                                            />
+                                            : <AccountCircleIcon color="primary" sx={{ width: 150, height: 150 }} />
+                                        }
+
                                         <Button component="label" variant="outlined" startIcon={<CloudUploadIcon />}>
                                             Upload Image
-                                            <VisuallyHiddenInput type="file" />
+                                            <VisuallyHiddenInput type="file" onChange={handleImageChange} />
                                         </Button>
-                                        <TextField sx={{ my: 3 }} required id="UserName" label="User Name" variant="standard" fullWidth />
-                                        <TextField sx={{ mb: 3 }} required id="UserName" label="Email" type="email" variant="standard" fullWidth />
-                                        <FormControl sx={{ width: "100%", mb: 3 }} variant="standard" required>
+                                        {!imageOk &&
+                                            <Alert severity="error">Please upload an image</Alert>
+                                        }
+                                        <TextField
+                                            onChange={handleNameChange}
+                                            sx={{ mt: 3 }}
+                                            required id="UserName"
+                                            label="User Name"
+                                            variant="standard"
+                                            fullWidth
+                                            error={!nameOk}
+                                        />
+                                        {!nameOk &&
+                                            <Alert sx={{ width: "100%" }} severity="error">Enter your username</Alert>
+                                        }
+                                        <TextField
+                                            sx={{ mt: 3 }}
+                                            onChange={handleEmailChange}
+                                            required id="Email"
+                                            label="Email"
+                                            type="email"
+                                            variant="standard"
+                                            fullWidth
+                                            error={!emailOk}
+                                        />
+                                        {!emailOk ?
+                                            <Alert sx={{ width: "100%" }} severity="error">Not a valid email</Alert>
+                                            :
+                                            email && <Alert sx={{ width: "100%" }} severity="success">Valid email</Alert>
+                                        }
+                                        <FormControl
+                                            sx={{ width: "100%", my: 3 }}
+                                            variant="standard"
+                                            required
+                                            error={!passwordOk}
+                                        >
                                             <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
                                             <Input
+                                                onChange={handlePasswordChange}
                                                 id="standard-adornment-password"
                                                 type={showPassword ? 'text' : 'password'}
                                                 endAdornment={
@@ -71,8 +183,19 @@ const Signup = () => {
                                                     </InputAdornment>
                                                 }
                                             />
+                                            {!passwordOk ?
+                                                <Alert severity="error">Password does not meet minimum requirements</Alert>
+                                                :
+                                                password && <Alert severity="success">Valid password</Alert>
+                                            }
                                         </FormControl>
-                                        <Button sx={{ my: 3 }} type="submit" variant="contained" fullWidth>Create Account</Button>
+                                        <Button
+                                            sx={{ my: 3 }}
+                                            type="submit"
+                                            variant="contained"
+                                            fullWidth
+
+                                        >Create Account</Button>
                                     </form>
                                 </CardContent>
                             </Card>
